3e76a8e1d648e6cf370b908fee0fb1c35ebd01be
Gaur <7697343+sarincasm@users.noreply.github.com>
2021-04-14 23:05:04 +0530

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+/** @format */
+
+module.exports = {
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'eslint-config-prettier',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+	},
+	plugins: ['react'],
+	rules: {},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+}
