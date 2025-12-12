a6b2d33d536f353c245d1fb2e1a62904cc5fb76f
sarincasm <eelsweb@gmail.com>
2025-12-12 17:51:37 +0100

    finicky: Update configuration brewfile: update packages zshrc: cleanup

--- .finicky.js
+++ .finicky.js
@@ -4,99 +4,67 @@
  * Save as ~/.finicky.js
  */
 
-module.exports = {
-	defaultBrowser: 'Safari',
+export default {
+	defaultBrowser: {
+		name: 'Comet',
+		profile: 'eels',
+	},
+	// defaultBrowser: 'Safari',
 	handlers: [
 		{
-			// https://www.icloud.com/shortcuts/a466e6ecc5d44d8189e5b6e597f42d56
+			match: /^https?:\/\/google\.com\/.*$/,
+			browser: {
+				name: 'Brave Browser',
+				profile: 'P1',
+			},
+		},
+		{
 			match: /^https?:\/\/www\.google\.com\/.*$/,
 			browser: {
 				name: 'Brave Browser',
-				profile: 'Profile 4',
-			},
-		},
-		{
-			match: /^https?:\/\/.*\.youtube\.com\/.*$/,
-			browser: {
-				name: 'Brave Browser',
-				profile: 'Profile 3',
-			},
-		},
-		{
-			match: /^https?:\/\/youtu\.be\/.*$/,
-			browser: {
-				name: 'Brave Browser',
-				profile: 'Profile 3',
+				profile: 'P1',
 			},
 		},
 		{
 			match: /^https?:\/\/duckduckgo\.com\/.*$/,
 			browser: {
 				name: 'Brave Browser',
-				profile: 'Profile 4',
+				profile: 'P1',
 			},
 		},
 		{
-			match: /motain/,
+			match: /^https?:\/\/.*\.youtube\.com\/.*$/,
 			browser: {
 				name: 'Brave Browser',
-				profile: 'Profile 1',
+				profile: 'P2',
+			},
+		},
+		{
+			match: /^https?:\/\/youtu\.be\/.*$/,
+			browser: {
+				name: 'Brave Browser',
+				profile: 'P2',
+			},
+		},
+		{
+			match: /kertos/i,
+			browser: {
+				name: 'Comet',
+				profile: 'kertos',
+			},
+		},
+		{
+			match: /^https?:\/\/.*\.figma\.com\/.*$/,
+			browser: {
+				name: 'Comet',
+				profile: 'kertos',
 			},
 		},
 		{
 			match: /^https?:\/\/github\.com\/.*$/,
 			browser: {
-				name: 'Brave Browser',
-				profile: 'Profile 2',
-			},
-		},
-		{
-			match: /^https?:\/\/mail\.google\.com\/.*$/,
-			browser: {
-				name: 'Brave Browser',
-				profile: 'Profile 1',
-			},
-		},
-		{
-			match: /^https?:\/\/drive\.google\.com\/.*$/,
-			browser: {
-				name: 'Brave Browser',
-				profile: 'Profile 1',
-			},
-		},
-		{
-			match: /^https?:\/\/docs\.google\.com\/.*$/,
-			browser: {
-				name: 'Brave Browser',
-				profile: 'Profile 1',
-			},
-		},
-		{
-			match: /^https?:\/\/onefootball\.atlassian\.net\/.*$/,
-			browser: {
-				name: 'Brave Browser',
-				profile: 'Profile 1',
-			},
-		},
-		{
-			match: /onefootball/,
-			browser: {
-				name: 'Brave Browser',
-				profile: 'Profile 1',
-			},
-		},
-		{
-			match: /^https?:\/\/.*\.jumpcloud\.com\/.*$/,
-			browser: {
-				name: 'Brave Browser',
-				profile: 'Profile 1',
-			},
-		},
-		{
-			match: /^https?:\/\/.*\eu.mixpanel\.com\/.*$/,
-			browser: {
-				name: 'Brave Browser',
-				profile: 'Profile 1',
+				name: 'Comet',
+				profile: 'eels',
 			},
 		},
 	],
--- terminal/.zshrc
+++ terminal/.zshrc
@@ -9,56 +9,12 @@ PROMPT='😈😇%2~%#'
 # homebrew
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-#tmux
-alias lc="~/Documents/N/Eels/Repos/DevEx/terminal/lc"
-
-# cd using fzf
-alias sd="cd \$(find ~/Documents/N ~/Documents/CapJ -not -path '*/.*' -maxdepth 3 -type d | fzf)"
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# fzf
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-bindkey '^[[A' fzf-history-widget
-
-# autosuggestions
-source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-# thefuck
-eval $(thefuck --alias)
-
-# launch tmux
-if [ -z "$TMUX" ]
-then
-  tmux attach -t "HOME" || tmux new -s "HOME"  
-fi
-
-mamba_init() {
-  # >>> conda initialize >>>
-  # !! Contents within this block are managed by 'conda init' !!
-  __conda_setup="$('/opt/homebrew/Caskroom/miniforge/base/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-  if [ $? -eq 0 ]; then
-      eval "$__conda_setup"
-  else
-      if [ -f "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh" ]; then
-          . "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh"
-      else
-          export PATH="/opt/homebrew/Caskroom/miniforge/base/bin:$PATH"
-      fi
-  fi
-  unset __conda_setup
-
-  if [ -f "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/mamba.sh" ]; then
-      . "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/mamba.sh"
-  fi
-  # <<< conda initialize <<<
-}
-alias mamel="mamba env list"
-mamact() {
-  mamba activate $1
-}
-
-alias poetry="~/Library/Application\ Support/pypoetry/venv/bin/poetry"
